72a1c7120a90be41a1d06a76fb41686f6b24b2a0
lhzulkifli <milesray717@yahoo.com>
2020-10-26 15:20:04 +0800

    for programming class

--- basic.py
+++
@@ -1,9 +0,0 @@
-import math
-
-radius_str = input ('enter radius of circle:')
-radius_int = int(radius_str)
-circumference = 2*math.pi*radius_int
-area = math.pi*(radius_int**2)
-
-print ('circumference:', circumference, \
-       'and area:', area)
